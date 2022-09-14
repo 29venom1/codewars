d1a01a4de3854e4a66f31e40aa845e6528401697
zre12f <zre12f@gmail.com>
2022-09-14 15:40:38 +0300

    added "Highest and Lowest"

---
+++ 7_kyu/Highest and Lowest.py
@@ -0,0 +1,5 @@
+def high_and_low(numbers):
+    # return ' '.join([i for i in numbers.split() if i == min(numbers.split()) or i == max(numbers.split())][::-1])
+
+
+print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
