8f4451e5e340af9de80c981a23e0edb02c4d0722
zre12f <zre12f@gmail.com>
2022-09-14 14:00:14 +0300

    List Filtering solution

--- 7_kyu/List Filtering.py
+++ 7_kyu/List Filtering.py
@@ -1,4 +1,7 @@
 # In this kata you will create a function that takes a list of non-negative integers
 # and strings and returns a new list with the strings filtered out.
 def filter_list(l):
-    return
+    print([i for i in l if type(i) == int])
+
+
+filter_list([1, 2, 'aasf', '1', '123', 123])
