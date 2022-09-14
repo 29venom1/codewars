86804ac97f7a18326093623987f247813c12d8ae
zre12f <zre12f@gmail.com>
2022-09-14 11:50:27 +0300

    "Your order, please" solution"
    exit
    fjsd
    
    git status

--- 6_kyu/Your order, please.py
+++ 6_kyu/Your order, please.py
@@ -1,3 +1,15 @@
 def order(sentence):
-  # code here
-  return
+  d ={i+1:v for i in list(range(len(sentence.split()))) for v in sentence.split() if str(i+1) in v}
+  return(' '.join([d[l] for l in d]))
+  # one string method:
+  # return(' '.join([{i+1:v for i in list(range(len(sentence.split())))
+  #       for v in sentence.split() if str(i+1) in v}[l]
+  #       for l in {i+1:v for i in list(range(len(sentence.split())))
+  #       for v in sentence.split() if str(i+1) in v}]))
+
+  # one more method:
+  # return ' '.join(sorted(words.split(), key=lambda w:sorted(w)))
+
+print(order("4of Fo1r pe6ople g3ood th5e the2"))
+
+
