7360c9aa5f33915ca1cd07a5a2ce0cd064115664
zre12f <zre12f@gmail.com>
2022-09-14 12:48:56 +0300

    Are You Playing Banjo solution

--- 8_kyu/Are You Playing Banjo.py
+++ 8_kyu/Are You Playing Banjo.py
@@ -1,3 +1,9 @@
 def are_you_playing_banjo(name):
-    # Implement me!
-    return name
+    # Create a function which answers the question "Are you playing banjo?".
+    # If your name starts with the letter "R" or lower case "r", you are playing banjo!
+    return name + ' ' + 'plays banjo' if name[0] == 'R' or name[0] == 'r' else name + ' ' + 'does not play banjo'
+    # other solution:
+    # return name + (' plays' if name[0].lower() == 'r' else ' does not play') + " banjo"
+
+
+print(are_you_playing_banjo("rolf"))
